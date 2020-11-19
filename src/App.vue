e61412c99dc71a983de6e4f732d5dd51100cc0b6
Nathan Reyes <nreyes@Nathans-MacBook-Pro.local>
2020-11-18 21:49:50 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,60 @@
 <template>
   <div>
-    <Calendar :attributes="attributes" />
+    <Calendar :attributes="attributes">
+      <template #day-popover="{}"> Helloooo </template>
+    </Calendar>
   </div>
-  <div>
-    <DatePicker v-model="date" />
+  <div class="date-examples">
+    <div>
+      <div>
+        <label for="dark">
+          <input id="dark" type="checkbox" v-model="isDark" />
+          Is Dark</label
+        >
+        <label for="range">
+          <input id="range" type="checkbox" v-model="isRange" />
+          Is Range</label
+        >
+      </div>
+      <div>
+        <label for="date">
+          <input id="date" type="radio" v-model="mode" value="date" />
+          date
+        </label>
+        <label for="datetime">
+          <input id="datetime" type="radio" v-model="mode" value="datetime" />
+          datetime</label
+        >
+        <label for="time">
+          <input id="time" type="radio" v-model="mode" value="time" />
+          time</label
+        >
+      </div>
+      <DatePicker
+        :mode="mode"
+        v-model="date"
+        :is-dark="isDark"
+        :is-range="isRange"
+      />
+    </div>
+    <div>
+      <DatePicker
+        :mode="mode"
+        v-model="date"
+        :is-dark="isDark"
+        :is-range="isRange"
+      >
+        <template #default="{ inputValue, inputEvents }">
+          <template v-if="isRange">
+            <input :value="inputValue.start" v-on="inputEvents.start" />
+            <input :value="inputValue.end" v-on="inputEvents.end" />
+          </template>
+          <template v-else>
+            <input :value="inputValue" v-on="inputEvents" />
+          </template>
+        </template>
+      </DatePicker>
+    </div>
   </div>
 </template>
 
@@ -20,21 +71,37 @@ export default {
   data() {
     return {
       date: new Date(),
+      mode: 'datetime',
+      isDark: false,
+      isRange: false,
       attributes: [
-        { highlight: true, dates: [new Date(), new Date(2020, 10, 12)] },
+        {
+          highlight: true,
+          popover: {
+            label: 'Testing',
+          },
+          dates: [new Date(), new Date(2020, 10, 12)],
+        },
       ],
     };
+  },
+  watch: {
+    isRange(val) {
+      if (val) {
+        this.date = {
+          start: new Date(2020, 10, 16),
+          end: new Date(2020, 10, 20),
+        };
+      } else {
+        this.date = new Date();
+      }
+    },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.date-examples {
+  display: flex;
 }
 </style>

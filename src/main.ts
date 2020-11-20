5380c47fbc6fdb16ecb412b040445aa5d2c3da98
Nathan Reyes <nathanreyes@me.com>
2020-11-19 21:19:47 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+const defaults = {
+  titlePosition: 'right',
+};
+
+// import VCalendar from './index';
+// app.use(VCalendar, defaults);
+
+window.__vcalendar__ = defaults;
+
+import { screensPlugin, Calendar, DatePicker } from './index';
+console.log(screensPlugin);
+app.use(screensPlugin);
+app.use(Calendar);
+app.use(DatePicker);
+
+app.mount('#app');
